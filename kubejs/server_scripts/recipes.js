d7340c7aea60d6741c108c690efca900897408f5
Raeffi <raeffi@gmx.at>
2024-05-11 13:58:22 +0200

    Updated Recipes / Block Temperature
    Added More Js

--- kubejs/server_scripts/recipes.js
+++ kubejs/server_scripts/recipes.js
@@ -106,7 +106,24 @@ e.recipes.create.mechanical_crafting( "cold_sweat:soulspring_lamp", [
     S: "create:sturdy_sheet",
     P: "#forge:plates/steel",    
     G: "#forge:glass_panes",
-    H: "minecraft:blue_ice"
+    H: "cold_sweat:soul_sprout"
+})
+
+e.remove({output: "cold_sweat:hearth"})
+
+e.recipes.create.mechanical_crafting( "cold_sweat:hearth", [
+    ' WPW ',
+    ' WPW ',
+    'CCFCC',
+    'CTHTC',
+    'CCCCC'
+], {
+    P: "create:fluid_pipe",
+    C: "create:cut_scoria",
+    W: "minecraft:cobblestone_wall",
+    H: "create_things_and_misc:sprinkler",
+    F: "create:propeller",
+    T: "create:fluid_tank"
 })
 
 //####################################################################################
@@ -170,15 +187,60 @@ e.recipes.create.sequenced_assembly(['cgm:shell'], "#forge:plates/brass", [
 
 e.recipes.create.compacting( "minecraft:ice" , "8x minecraft:snow_block")
 
+e.remove({output: "create:sturdy_sheet"})
+
+inter = "create:unprocessed_obsidian_sheet"
+
+e.recipes.create.sequenced_assembly(["create:sturdy_sheet"], "#forge:dusts/obsidian", [
+    e.recipes.createFilling(inter, [inter, Fluid.lava(500)]),
+    e.recipes.createPressing(inter, inter),
+    e.recipes.createDeploying(inter, [inter, "#forge:plates/steel"]),
+    e.recipes.createPressing(inter, inter)
+]).transitionalItem(inter).loops(1)
+
+e.remove({output: "create:precision_mechanism"})
+
+inter = "create:incomplete_precision_mechanism"
+
+e.recipes.create.sequenced_assembly([
+    Item.of('create:precision_mechanism').withChance(130.0), // this is the item that will appear in JEI as the result
+    Item.of('create:golden_sheet').withChance(8.0), // the rest of these items will be part of the scrap
+    Item.of('create:andesite_alloy').withChance(8.0),
+    Item.of('create:cogwheel').withChance(5.0),
+    Item.of('create:shaft').withChance(2.0),
+    Item.of('create:crushed_gold_ore').withChance(2.0),
+    Item.of('2x gold_nugget').withChance(2.0),
+    'iron_ingot',
+    'clock'
+], 'create:golden_sheet', [ // 'create:golden_sheet' is the input
+    // the transitional item set by `transitionalItem('create:incomplete_large_cogwheel')` is the item used during the intermediate stages of the assembly
+    e.recipes.createDeploying(inter, [inter, 'create:cogwheel']),
+    // like a normal recipe function, is used as a sequence step in this array. Input and output have the transitional item
+    e.recipes.createDeploying(inter, [inter, 'create:large_cogwheel']),
+    e.recipes.createDeploying(inter, [inter, '#forge:nuggets/steel'])
+]).transitionalItem(inter).loops(5) // set the transitional item and the number of loops
 
 
 //####################################################################################
 
 //PNEUMATICRAFT RECIPES
-
-
+e.replaceInput(
+    [{ output: "pneumaticcraft:pressure_chamber_wall" },{ output: "pneumaticcraft:pressure_chamber_glass" }],
+    "pneumaticcraft:reinforced_bricks",
+    "create:sturdy_sheet"
+  )
+e.replaceInput(
+    { output: "pneumaticcraft:pressure_chamber_valve" },
+    "#forge:ingots/compressed_iron",
+    "create:sturdy_sheet"
+  )
 
 //####################################################################################
 
+//IE RECIPES
+
+e.recipes.create.mixing("minecraft:tuff", '#forge:slag').superheated()
+
+//####################################################################################
 })
 
